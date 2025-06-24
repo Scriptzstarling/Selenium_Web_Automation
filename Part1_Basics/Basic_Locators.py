f4521698636cd551131254014de53afee238be57
Scriptzstarling <shashwat11muz@gmail.com>
2025-06-24 13:21:37 +0530

    update with using XPATH

--- Part1_Basics/Basic_Locators.py
+++ Part1_Basics/Basic_Locators.py
@@ -14,16 +14,27 @@ driver.get("https://www.saucedemo.com")
 time.sleep(2) #wait for 2 second to let the page load
 
 #locate username web element
-username = driver.find_element(By.ID, "user-name")
+# username = driver.find_element(By.NAME, "user-name")
+
+#relative xpath to locate username
+username = driver.find_element(By.XPATH, "//input[@id='user-name']")
 
 #enter username
 username.send_keys("standard_user")
 
 #locate password web element
-password = driver.find_element(By.ID, "password")
+# password = driver.find_element(By.ID, "password")
+password = driver.find_element(By.XPATH, "//input[@id='password']")
 
 #enter password
 password.send_keys("secret_sauce")
+time.sleep(5)
+
+#locate login button
+loginBtn = driver.find_element(By.XPATH, "//input[@id='login-button']")
+
+#click on login button
+loginBtn.click()
 time.sleep(5)
 
 
